4f31af7dd0962c092da217c3c5ff816dfef5ad8d
douglas <douglas06mp@gmail.com>
2025-05-19 19:15:25 +0800

    chore: pest

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+
+class UserRepository
+{
+    /**
+     * Get all users.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection<User>
+     */
+    public function getAllUsers()
+    {
+        return User::all();
+    }
+
+    /**
+     * Find a user by ID.
+     *
+     * @param  int  $id
+     * @return \App\Models\User|null
+     */
+    public function findUserById($id)
+    {
+        return User::find($id);
+    }
+
+    /**
+     * Create a new user.
+     *
+     * @param  array  $data
+     * @return \App\Models\User
+     */
+    public function createUser(array $data)
+    {
+        return User::create($data);
+    }
+}
---
+++ tests/Unit/UserRepositoryTest.php
@@ -0,0 +1,17 @@
+<?php
+
+use Tests\TestCase;
+use App\Repositories\UserRepository;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(TestCase::class, RefreshDatabase::class);
+
+beforeEach(function () {
+    $this->userRepository = new UserRepository();
+});
+
+it('can get all users', function () {
+    $users = $this->userRepository->getAllUsers();
+
+    expect($users)->toBeInstanceOf(\Illuminate\Database\Eloquent\Collection::class);
+});
